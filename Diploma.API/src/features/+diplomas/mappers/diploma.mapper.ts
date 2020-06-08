84607c706785dd248d7bf18f282f17d7ecf53682
mylokhindima <dmytro.mylokhin@itechcraft.com>
2020-06-08 19:08:47 +0300

    fixes

--- Diploma.API/src/features/+diplomas/mappers/diploma.mapper.ts
+++ Diploma.API/src/features/+diplomas/mappers/diploma.mapper.ts
@@ -15,28 +15,34 @@ import { reportMapper } from './report.mapper';
 export function diplomaMapper(diploma: DiplomaDocument): DiplomaEntity {
     const partial = pick(diploma, ['theme']) as any as Partial<DiplomaEntity>;
 
-    if (diploma.instructor && diploma.populated('instructor')) {
-        partial.instructor = professorMapper(diploma.instructor as ProfessorDocument);
-        partial.instructorId = partial.instructor.id;
-    } else {
-        partial.instructor = null;
-        partial.instructorId = diploma.instructor?.toString();
+    if (diploma.instructor) {
+        if (diploma.populated('instructor')) {
+            partial.instructor = professorMapper(diploma.instructor as ProfessorDocument);
+            partial.instructorId = partial.instructor.id;
+        } else {
+            partial.instructor = null;
+            partial.instructorId = diploma.instructor?.toString();
+        }
     }
 
-    if (diploma.student && diploma.populated('student')) {
-        partial.student = studentMapper(diploma.student as StudentDocument);
-        partial.studentId = partial.student.id;
-    } else {
-        partial.student = null;
-        partial.studentId = diploma.student.toString();
+    if (diploma.student) {
+        if (diploma.populated('student')) {
+            partial.student = studentMapper(diploma.student as StudentDocument);
+            partial.studentId = partial.student.id;
+        } else {
+            partial.student = null;
+            partial.studentId = diploma.student.toString();
+        }
     }
 
-    if (diploma.populated('stage')) {
-        partial.stage = stageMapper(diploma.stage as StageDocument);
-        partial.stageId = partial.stage.id;
-    } else {
-        partial.stage = null;
-        partial.stageId = diploma.stage.toString();
+    if (diploma.stage) {
+        if (diploma.populated('stage')) {
+            partial.stage = stageMapper(diploma.stage as StageDocument);
+            partial.stageId = partial.stage.id;
+        } else {
+            partial.stage = null;
+            partial.stageId = diploma.stage.toString();
+        }
     }
 
     if (diploma.reports && diploma.populated('reports')) {
